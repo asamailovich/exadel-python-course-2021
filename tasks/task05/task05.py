9cd1c470c55a5e02214c866706089f0b9396552d
Samailovich Andrei <asamailovich@exadel.net>
2021-09-27 22:56:51 +0300

    task05 fix commit

--- tasks/task05/task05.py
+++ tasks/task05/task05.py
@@ -1,31 +1,26 @@
-def create_user(name, surname, **kwargs) -> dict:
-    res = {name: name, surname: surname, "age": 42, "extra": {}}
-    for key, value in kwargs.items():
-        if key == 'age':
-            res[key] = value
-        else:
-            res["extra"][key] = value
+def create_user(name, surname, age=42, **kwargs) -> dict:
+    res = {"name": name, "surname": surname, "age": age, "extra": kwargs}
     return res
 
 
 assert create_user("John", "Doe") == \
        {
-           'John': 'John',
-           'Doe': 'Doe',
+           'name': 'John',
+           'surname': 'Doe',
            'age': 42,
            'extra': {}
        }
 assert create_user("John", "Doe", age=65) == \
        {
-           'John': 'John',
-           'Doe': 'Doe',
+           'name': 'John',
+           'surname': 'Doe',
            'age': 65,
            'extra': {}
        }
 assert create_user("John", "Doe", age=66, occupation="physicist", won_nobel=True) == \
        {
-           'John': 'John',
-           'Doe': 'Doe',
+           'name': 'John',
+           'surname': 'Doe',
            'age': 66,
            'extra': {
                'occupation': 'physicist',
